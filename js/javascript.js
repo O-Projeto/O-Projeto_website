35401d025990d89920f885b378919bed2b010a50
Luisa_Melo <97964206+Melo-Luisa@users.noreply.github.com>
2023-09-24 23:45:19 -0300

    upload all files
    JS - complete
    CSS - Complete and reponsive

---
+++ js/javascript.js
@@ -0,0 +1,113 @@
+//CARROSSEIS
+function carrosseis(){
+
+$(document).ready(function () {
+    const slider = $(".person");
+    const slider_obj = $(".comunidade");
+    const prevButton = $("#esquerda");
+    const nextButton = $("#direita");
+
+    let currentSlide = 0;
+    let autoRotationInterval;
+
+    function goToSlide(slideIndex) {
+        currentSlide = slideIndex;
+        const translateX = -currentSlide * 250; // 120px é a largura de cada quadrado
+        slider.css("transform", `translateX(${translateX}px)`);
+        slider_obj.css("transform", `translateX(${translateX}px)`);
+    }
+//pessoas
+    function startAutoRotation() { //inicia rotação 
+        autoRotationInterval = setInterval(function () {
+            if (currentSlide <= 180 ) { // tamanho dos quarados
+                goToSlide(currentSlide + 2);
+            } else {
+                // Ir suavemente de volta para o primeiro quadrado
+                slider.css("transition", "none");
+                goToSlide(0);
+                setTimeout(function () {
+                    slider.css("transition", "transform 0.5s ease-in-out");
+                }, 0);
+            }
+        }, 2000); // Altere o valor para ajustar a velocidade da rotação
+    }
+
+    //obj
+    function startAutoRotation_obj() { //inicia rotação 
+        autoRotationInterval = setInterval(function () {
+            if (currentSlide <= 15  ) { // tamanho dos quarados
+                goToSlide(currentSlide + 1);
+            } else {
+                // Ir suavemente de volta para o primeiro quadrado
+                slider_obj.css("transition", "none");
+                goToSlide(0);
+                setTimeout(function () {
+                    slider_obj.css("transition", "transform 0.5s ease-in-out");
+                }, 0);
+            }
+        }, 2000); // Altere o valor para ajustar a velocidade da rotação
+    }
+
+    function stopAutoRotation() {
+        clearInterval(autoRotationInterval);
+    }
+
+    prevButton.click(function () { //seta esqr
+        stopAutoRotation();
+        if (currentSlide > 0) {
+            goToSlide(currentSlide - 1);
+        } else {
+            // Ir suavemente para o último quadrado
+            slider.css("transition", "none");
+            goToSlide(8);
+            setTimeout(function () {
+                slider.css("transition", "transform 0.5s ease-in-out");
+            }, 0);
+        }
+        startAutoRotation();
+    });
+
+    nextButton.click(function () { // seta dire
+        stopAutoRotation();
+        if (currentSlide < 35) {
+            goToSlide(currentSlide + 1);
+        } else {
+            // Ir suavemente de volta para o primeiro quadrado
+            slider.css("transition", "none");
+            goToSlide(0);
+            setTimeout(function () {
+                slider.css("transition", "transform 0.5s ease-in-out");
+            }, 0);
+        }
+        startAutoRotation();
+    });
+
+    startAutoRotation();
+    startAutoRotation_obj();
+});
+
+}
+//EMAIL
+
+function email(){
+    document.getElementById("patrocinio_button").addEventListener("click", function() {
+        // Endereço de email para o destinatário
+        var destinatario = "trocar.ideia.projeto@gmail.com";
+
+        // Assunto do email
+        var assunto = "Parcerias com O PROJETO";
+
+        // Corpo do email
+        var corpoEmail = "";
+
+        // Monta o link de email com os dados
+        var linkEmail = "mailto:" + destinatario +
+                        "?subject=" + encodeURIComponent(assunto) +
+                        "&body=" + encodeURIComponent(corpoEmail);
+
+        // Abre o cliente de email padrão com os dados preenchidos
+        window.location.href = linkEmail;
+    });
+}
+carrosseis();
+email();
