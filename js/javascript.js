98c2cce2e10a686003b9428d21e34159b80a329f
Alexandre Calil <alexandrecalilmf@gmail.com>
2024-02-04 00:40:50 +0000

    feat: add menubar mobile

--- js/javascript.js
+++ js/javascript.js
@@ -1,96 +1,160 @@
-//CARROSSEIS
-function carrosseis(){
+// #region Menubar
+function toogleNavbar() {
+    const body = document.querySelector('body')
+    const navbarContent = document.getElementById('navbar-content')
+    const isOpen = navbarContent.classList.contains('open')
 
-$(document).ready(function () {
-    const slider = $(".person");
-    const slider_obj = $(".comunidade");
-    const prevButton = $("#esquerda");
-    const nextButton = $("#direita");
-
-    let currentSlide = 0;
-    let autoRotationInterval;
-
-    function goToSlide(slideIndex) {
-        currentSlide = slideIndex;
-        const translateX = -currentSlide * 120; // 120px é a largura de cada quadrado
-        slider.css("transform", `translateX(${translateX}px)`);
-        slider_obj.css("transform", `translateX(${translateX}px)`);
+    if (isOpen) {
+        navbarContent.classList.remove('open')
+        body.classList.remove('no-scroll')
+    } else {
+        navbarContent.classList.add('open')
+        body.classList.add('no-scroll')
     }
-//pessoas
-    function startAutoRotation() { //inicia rotação 
-        autoRotationInterval = setInterval(function () {
-            if (currentSlide <= 8 ) { // tamanho dos quarados
+}
+
+function navigateTo(index) {
+    const links = document.querySelectorAll('nav #navbar-content ul li a');
+    Array.from(links).forEach((link, linkIndex) => {
+        link.classList.remove('active');
+        if (linkIndex === index) {
+            link.classList.add('active');
+        }
+    })
+    const isMobile = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) < 768;
+    if (isMobile) {
+        toogleNavbar();
+    }
+}
+
+// FIXME: Mark the menubar item as active as scroll
+/*
+document.addEventListener('DOMContentLoaded', _ => {
+    const links = document.querySelectorAll('nav #navbar-content ul li a');
+
+    const getActiveSection = () => {
+        let activeSection = '';
+        const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+        links.forEach((item) => {
+            const section = document.querySelector(item.getAttribute('href'));
+            if (section.offsetTop <= scrollPosition && (section.offsetTop + section.offsetHeight) > scrollPosition) {
+                activeSection = item.getAttribute('href');
+            }
+        });
+
+        return activeSection;
+    };
+
+    const setActiveMenuItem = () => {
+        const activeSection = getActiveSection();
+
+        links.forEach((item) => {
+            if (item.getAttribute('href') === activeSection) {
+                item.classList.add('active');
+            } else {
+                item.classList.remove('active');
+            }
+        });
+    };
+
+    window.addEventListener('scroll', setActiveMenuItem);
+});
+*/
+// #endregion
+
+// #region Carroseis
+function carrosseis() {
+    $(document).ready(function () {
+        const slider = $(".person");
+        const slider_obj = $(".comunidade");
+        const prevButton = $("#esquerda");
+        const nextButton = $("#direita");
+
+        let currentSlide = 0;
+        let autoRotationInterval;
+
+        function goToSlide(slideIndex) {
+            currentSlide = slideIndex;
+            const translateX = -currentSlide * 120; // 120px é a largura de cada quadrado
+            slider.css("transform", `translateX(${translateX}px)`);
+            slider_obj.css("transform", `translateX(${translateX}px)`);
+        }
+        //pessoas
+        function startAutoRotation() { //inicia rotação 
+            autoRotationInterval = setInterval(function () {
+                if (currentSlide <= 8) { // tamanho dos quarados
+                    goToSlide(currentSlide + 1);
+                } else {
+                    // Ir suavemente de volta para o primeiro quadrado
+                    slider.css("transition", "none");
+                    goToSlide(0);
+                    setTimeout(function () {
+                        slider.css("transition", "transform 0.1s ease-in-out");
+                    }, 0);
+                }
+            }, 2000); // Altere o valor para ajustar a velocidade da rotação
+        }
+
+        //obj
+        function startAutoRotation_obj() { //inicia rotação 
+            autoRotationInterval = setInterval(function () {
+                if (currentSlide <= 8) { // tamanho dos quarados
+                    goToSlide(currentSlide + 1);
+                } else {
+                    // Ir suavemente de volta para o primeiro quadrado
+                    slider_obj.css("transition", "none");
+                    goToSlide(0);
+                    setTimeout(function () {
+                        slider_obj.css("transition", "transform 0.7s ease-in-out");
+                    }, 0);
+                }
+            }, 2000); // Altere o valor para ajustar a velocidade da rotação
+        }
+
+        function stopAutoRotation() {
+            clearInterval(autoRotationInterval);
+        }
+
+        /*prevButton.click(function () { //seta esqr
+            stopAutoRotation();
+            if (currentSlide > 0) {
+                goToSlide(currentSlide - 1);
+            } else {
+                // Ir suavemente para o último quadrado
+                slider.css("transition", "none");
+                goToSlide(8);
+                setTimeout(function () {
+                    slider.css("transition", "transform 0.5s ease-in-out");
+                }, 0);
+            }
+            startAutoRotation();
+        });
+    
+        nextButton.click(function () { // seta dire
+            stopAutoRotation();
+            if (currentSlide < 35) {
                 goToSlide(currentSlide + 1);
             } else {
                 // Ir suavemente de volta para o primeiro quadrado
                 slider.css("transition", "none");
                 goToSlide(0);
                 setTimeout(function () {
-                    slider.css("transition", "transform 0.1s ease-in-out");
+                    slider.css("transition", "transform 0.5s ease-in-out");
                 }, 0);
             }
-        }, 2000); // Altere o valor para ajustar a velocidade da rotação
-    }
+            startAutoRotation();
+        });*/
 
-    //obj
-    function startAutoRotation_obj() { //inicia rotação 
-        autoRotationInterval = setInterval(function () {
-            if (currentSlide <= 8  ) { // tamanho dos quarados
-                goToSlide(currentSlide + 1);
-            } else {
-                // Ir suavemente de volta para o primeiro quadrado
-                slider_obj.css("transition", "none");
-                goToSlide(0);
-                setTimeout(function () {
-                    slider_obj.css("transition", "transform 0.7s ease-in-out");
-                }, 0);
-            }
-        }, 2000); // Altere o valor para ajustar a velocidade da rotação
-    }
-
-    function stopAutoRotation() {
-        clearInterval(autoRotationInterval);
-    }
-
-    /*prevButton.click(function () { //seta esqr
-        stopAutoRotation();
-        if (currentSlide > 0) {
-            goToSlide(currentSlide - 1);
-        } else {
-            // Ir suavemente para o último quadrado
-            slider.css("transition", "none");
-            goToSlide(8);
-            setTimeout(function () {
-                slider.css("transition", "transform 0.5s ease-in-out");
-            }, 0);
-        }
         startAutoRotation();
+        startAutoRotation_obj();
     });
-
-    nextButton.click(function () { // seta dire
-        stopAutoRotation();
-        if (currentSlide < 35) {
-            goToSlide(currentSlide + 1);
-        } else {
-            // Ir suavemente de volta para o primeiro quadrado
-            slider.css("transition", "none");
-            goToSlide(0);
-            setTimeout(function () {
-                slider.css("transition", "transform 0.5s ease-in-out");
-            }, 0);
-        }
-        startAutoRotation();
-    });*/
-
-    startAutoRotation();
-    startAutoRotation_obj();
-});
-
 }
-//EMAIL
+// #endregion
 
-function email(){
-    document.getElementById("patrocinio_button").addEventListener("click", function() {
+// #region Email
+(function email() {
+    document.getElementById("patrocinio_button").addEventListener("click", function () {
         // Endereço de email para o destinatário
         var destinatario = "trocar.ideia.projeto@gmail.com";
 
@@ -102,36 +166,11 @@ function email(){
 
         // Monta o link de email com os dados
         var linkEmail = "mailto:" + destinatario +
-                        "?subject=" + encodeURIComponent(assunto) +
-                        "&body=" + encodeURIComponent(corpoEmail);
+            "?subject=" + encodeURIComponent(assunto) +
+            "&body=" + encodeURIComponent(corpoEmail);
 
         // Abre o cliente de email padrão com os dados preenchidos
         window.location.href = linkEmail;
     });
-}
-
-function menu(){
-    var nav = document.getElementById("navbar")
-    nav.style.display = (nav.style.display === 'block') ? 'none' : 'block';
-
-}   
-
-function light_and_dark(){
-    var body = document.body;
-    var moonIcon = document.getElementById("moon");
-    var sunIcon = document.getElementById("sun");
-
-    body.classList.toggle('dark');
-
-    moonIcon.style.display = body.classList.contains('dark') ? 'none' : 'inline';
-    sunIcon.style.display = body.classList.contains('dark') ? 'inline' : 'none';
-    
-
-}
-
-
-//carrosseis();
-email();
-menu();
-light_and_dark();
-
+})();
+// #endregion
